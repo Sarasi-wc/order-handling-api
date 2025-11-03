9c63ff08583a070a8e9049c85811fd4e22fa7d29
Sarasi-wc <132940378+Sarasi-wc@users.noreply.github.com>
2025-11-03 14:38:57 +0530

    add OrderWorkflowTest

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Domain\Orders\Models\Order;
+use App\Domain\Orders\Enums\OrderStatus;
+
+class OrderFactory extends Factory
+{
+    protected $model = Order::class;
+
+    public function definition(): array
+    {
+        return [
+            'order_id'       => $this->faker->unique()->numberBetween(1000, 9999),
+            'customer_name'  => $this->faker->name(),
+            'customer_email' => $this->faker->safeEmail(),
+            'product_sku'    => strtoupper($this->faker->lexify('SKU###')),
+            'product_name'   => $this->faker->words(3, true),
+            'quantity'       => $this->faker->numberBetween(1, 3),
+            'unit_price'     => $this->faker->randomFloat(2, 1000, 20000),
+            'payment_method' => $this->faker->randomElement(['card', 'cash', 'bank']),
+            'order_date'     => now(),
+            'status'         => OrderStatus::PENDING,
+        ];
+    }
+}
