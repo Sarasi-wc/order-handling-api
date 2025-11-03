d7a4c6022557f8bba5a6f4826f3eb3c05ab5c8d3
Sarasi-wc <132940378+Sarasi-wc@users.noreply.github.com>
2025-11-03 05:41:56 +0530

    Create 2025_11_03_001026_create_orders_table.php

---
+++ database/migrations/2025_11_03_001026_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_id')->unique();
+            $table->string('customer_name');
+            $table->string('customer_email');
+            $table->string('product_sku');
+            $table->string('product_name');
+            $table->integer('quantity');
+            $table->decimal('unit_price', 10, 2);
+            $table->string('payment_method');
+            $table->timestamp('order_date')->nullable();
+            $table->string('status')->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
