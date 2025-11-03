1fc6b98997382769048fd35bd328de2a0cf7f4b5
Sarasi-wc <132940378+Sarasi-wc@users.noreply.github.com>
2025-11-03 18:17:26 +0530

    Update DomainServiceProvider.php

--- app/Providers/DomainServiceProvider.php
+++ app/Providers/DomainServiceProvider.php
@@ -4,6 +4,12 @@ namespace App\Providers;
 
 use App\Domain\Orders\Console\ImportOrdersCommand;
 use App\Domain\Orders\Console\TestOrderWorkflowCommand;
+use App\Domain\Orders\Events\OrderCompleted;
+use App\Domain\Orders\Events\OrderFailed;
+use App\Domain\Orders\Listeners\SendOrderCompletedNotification;
+use App\Domain\Orders\Listeners\SendOrderFailedNotification;
+use App\Domain\Orders\Listeners\UpdateKPIMetrics;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class DomainServiceProvider extends ServiceProvider
@@ -24,6 +30,16 @@ class DomainServiceProvider extends ServiceProvider
         $this->commands([
             ImportOrdersCommand::class,
             TestOrderWorkflowCommand::class,
+        ]);
+
+        // Register event listeners
+        Event::listen(OrderCompleted::class, [
+            UpdateKPIMetrics::class,
+            SendOrderCompletedNotification::class,
+        ]);
+
+        Event::listen(OrderFailed::class, [
+            SendOrderFailedNotification::class,
         ]);
     }
 }
